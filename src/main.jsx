f173887f014283f95003bdbd832421a652118a86
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2023-10-16 20:41:28 +0700

    Inialisasi awal Project beserta Struktur Folder Project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LandingPage from "./pages/landingpage/landingpage";
+import "../src/styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
---
+++ src/pages/landingpage/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <p>Siap Lapor</p>
+    </div>
+  );
+}
